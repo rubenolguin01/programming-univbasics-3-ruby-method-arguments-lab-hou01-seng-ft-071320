031f479bfabaee791f58d151c0bd4c54d7629fe0
Ruben Israel Olguin <rubenolguin01@gmail.com>
2020-07-06 02:47:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,8 +10,11 @@ end
 
 introduction_with_language("Ruben","Ruby")
 
-def introduction_with_language_optional
-  
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Ruben","Ruby")
 
 
 
